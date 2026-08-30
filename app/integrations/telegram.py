0ad4591b122d9f97f693f3c36611b7da4a06f9e4
lucasbatista2408 <lucasbatista2408@gmail.com>
2026-08-30 18:43:14 -0300

    feat: add Telegram integration

---
+++ app/integrations/telegram.py
@@ -0,0 +1,30 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_message(message):
+    url = (
+        f"https://api.telegram.org/bot"
+        f"{TELEGRAM_BOT_TOKEN}/sendMessage"
+    )
+
+    response = requests.post(
+        url,
+        data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message
+        }
+    )
+
+    response.raise_for_status()
+
+    return response.json()
