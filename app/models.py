627fbeafd6035ddf7e3e13af42a248be07c5c209
lucasbatista2408 <lucasbatista2408@gmail.com>
2026-08-30 14:23:10 -0300

    refactor: preserve product id during scraping

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 class Product:
-    def __init__(self, url, target_price, name = None, price=None, id=None):
+    def __init__(self, url, target_price, name = None, price=None, id=None, image_url=None):
         self.id = id
         self.name = name
         self.url = url
         self.target_price = target_price
         self.price = price
+        self.image_url = image_url
--- app/scraper/amazon.py
+++ app/scraper/amazon.py
@@ -11,14 +11,23 @@ def amazon_scraper(product):
             page.goto(product.url)
 
             price = page.locator(".priceToPay")
-
-            title = page.locator("#productTitle").inner_text()
+            title = page.locator("#productTitle")
+            image = page.locator("#landingImage")
 
             if price.count() > 0:
                 price_value = get_price(price)
+                title_value = title.inner_text()
 
-                product.name = title
+                if image.count() > 0:
+                    image_url = image.get_attribute("data-old-hires")
+                else:
+                    image_url = None
+
+                product.name = title_value
                 product.price = price_value
+
+                # Ainda não temos image_url no model.
+                # product.image_url = image_url
 
                 browser.close()
 
